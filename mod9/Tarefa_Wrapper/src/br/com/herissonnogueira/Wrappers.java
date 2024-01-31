91f61749e6eed1df82ab3d5acb5ff3a348a1d996
Herisson Nogueira <herisson.nogueira002@gmail.com>
2024-01-31 18:39:34 -0300

    Finalizando o exercício do módulo 9

---
+++ mod9/Tarefa_Wrapper/src/br/com/herissonnogueira/Wrappers.java
@@ -0,0 +1,16 @@
+package br.com.herissonnogueira;
+
+/**
+ * @author herisson.nogueira
+ */
+
+
+public class Wrappers {
+    public static void main(String[] args) {
+        int valor = 10;
+        System.out.println(valor);
+
+        Integer wrapperValor = valor;
+        System.out.println(wrapperValor);
+    }
+}
